408dbf296b486896df6dffb29d0204d67d149ead
Maks <maks1makrov@gmail.com>
2020-10-09 01:30:47 +0300

    add bookadmin

--- managebook/admin.py
+++ managebook/admin.py
@@ -2,5 +2,15 @@ from django.contrib import admin
 
 from managebook.models import Book, Genre
 
-admin.site.register(Book)
+
+class BookAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    list_display = ['title', 'publish_date']
+    search_fields = ['title']
+    list_filter = ['publish_date', 'author', "genre"]
+    readonly_fields = ["cached_rate"]
+
+
+admin.site.register(Book, BookAdmin)
 admin.site.register(Genre)
+
